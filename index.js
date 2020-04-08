c7e01e0eddfed34a5805125d2dd64c84dc22b1a0
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-08 19:55:46 -0400

    completed userRouter.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(3000, () => {
+server.listen(4000, () => {
   console.log('\n* Server Running on http://localhost:4000 *\n');
 });
